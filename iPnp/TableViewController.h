73491479a47596e6fe7735e52d673cebaaa3b01c
Franco Cedillo <franco.cedillo@gmail.com>
2014-10-03 18:02:16 -0500

    adds tableview

---
+++ iPnp/TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TableViewController.h
+//  iPnp
+//
+//  Created by Franco Cedillo on 10/3/14.
+//  Copyright (c) 2014 HartasCuerdas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewController : UITableViewController
+
+@end
